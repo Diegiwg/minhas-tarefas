6f27c49597d515da9f157ca8f481f99abed84091
Diegiwg <diegiwg@gmail.com>
2023-05-03 01:57:26 -0300

    Configuração das Libs de JWT e Criptografia

--- api/main.py
+++ api/main.py
@@ -1,12 +1,17 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
 
 from .data import config
 from .models import task, user  # noqa: F401 - Register models on database
 
 load_dotenv()
-
 config.exec()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 app = FastAPI()
 
 
